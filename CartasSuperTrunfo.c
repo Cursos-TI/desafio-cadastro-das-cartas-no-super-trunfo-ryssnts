0aaa44ab83cee5604ac5020ef57a7233d4ebd24c
ryssnts <ryan999boy@yahoo.com.br>
2025-03-22 15:34:49 +0000

    CRIEI O PRINTF

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main(){
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+char estado1;
+char codigo_estado1 [5];
+char nome_cidade1[40];
+int populacao1, pontos_turisticos1;
+float area1, pib1;
 
-    return 0;
+char estado2;
+char codigo_estado2[5];
+char nome_cidade2[40];
+ int populacao2, pontos_turisticos2;
+float area2, pib2;
+
+printf("\n**CADASTRO DE CARTAS**\n\n");
+
+printf("Olá! Vamos começar a cadastrar as suas cartas para o jogo Super Trunfo!\nPara isso, precisamos de algumas informações...\n\n");
+printf("*CARTA 01*\n");
+printf("Digite a primeira letra do estado: \n");
+scanf(" %c", &estado1);
+
+printf("Digite o código da carta: *Adicione a primeira letra do estado + 01, por exemplo\n");
+scanf(" %s", &codigo_estado1);
+
+printf("Digite a cidade que pertence a este estado: \n");
+scanf(" %s", &nome_cidade1);
+
+printf("Qual o número de habitantes da cidade?: \n");
+scanf(" %d", &populacao1);
+
+printf("Qual o tamanho desta cidade?: \n");
+scanf(" %f", &area1);
+
+printf("PIB da cidade: \n");
+scanf(" %f", &pib1);
+
+printf("Quantos pontos turísticos esta cidade tem?: ");
+scanf(" %d", &pontos_turisticos1);
+
+printf("\n\nAgora vamos cadastrar a sua segunda carta!\n\n");
+
+printf("*CARTA 02*\n");
+printf("Digite a primeira letra do estado: \n");
+scanf(" %c", &estado2);
+
+printf("Digite o código da carta: *Adicione a primeira letra do estado + 01, por exemplo\n");
+scanf(" %s", &codigo_estado2);
+
+printf("Digite a cidade que pertence a este estado: \n");
+scanf(" %s", &nome_cidade2);
+
+printf("Qual o número de habitantes da cidade?: \n");
+scanf(" %d", &populacao2);
+
+printf("Qual o tamanho desta cidade?: \n");
+scanf(" %f", &area2);
+
+printf("PIB da cidade: \n");
+scanf(" %f", &pib2);
+
+printf("Quantos pontos turísticos esta cidade tem?: \n");
+scanf(" %d", &pontos_turisticos2);
+
+printf("\n\n**RESULTADO**\n\n");
+
+printf("**CARTA01**\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f\nPIB: %f\nNúmero de Pontos Turísticos: %d\n\n", estado1, codigo_estado1, nome_cidade1, populacao1,area1, pib1, pontos_turisticos1);
+printf("**CARTA02**\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %f\nPIB: %f\nNúmero de Pontos Turísticos: %d\n\n", estado2, codigo_estado2, nome_cidade2, populacao2, area2, pib2, pontos_turisticos2);
+
+return 0;
 }
